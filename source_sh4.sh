14b033a4627b52cfe10596ef518d822e549ad7b3
Rémi Duraffort <remi.duraffort@st.com>
2011-04-22 10:31:55 +0200

    sh4: add the need file to compile.

---
+++ source_sh4.sh
@@ -0,0 +1,9 @@
+export TOOL_PREFIX=/sw/st/gnu_compil/comp/st40/st40-linux/4.2.4-66/sh4/bin/sh4-linux
+export CXX=$TOOL_PREFIX-g++
+export AR=$TOOL_PREFIX-ar
+export RANLIB=$TOOL_PREFIX-ranlib
+export CC=$TOOL_PREFIX-cc
+export LD=$TOOL_PREFIX-ld
+
+export SH4_TARGET_ROOT=/sw/st/gnu_compil/comp/st40/st40-linux/4.2.4-66/sh4/target
+export CCFLAGS=
