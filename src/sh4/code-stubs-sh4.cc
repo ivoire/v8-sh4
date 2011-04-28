d404065e9a82a17b019e273f18b3d4270d16087b
Christophe Guillon <christophe.guillon@st.com>
2011-04-28 10:50:15 +0200

    sh4: implementation of GenerateNewObject().

--- src/sh4/code-stubs-sh4.cc
+++ src/sh4/code-stubs-sh4.cc
@@ -38,7 +38,352 @@ namespace internal {
 
 
 void ArgumentsAccessStub::GenerateNewObject(MacroAssembler* masm) {
-  UNIMPLEMENTED();
+  // sp[0] : number of parameters
+  // sp[4] : receiver displacement
+  // sp[8] : function
+
+  // live-in registers: none
+  // defined registers (live through the whole function): sp, fp, cp
+
+  // Registers map arm -> st40
+  // r1 -> r1
+  // r2 -> r2
+  // r3 -> any reg available in {r0, r1, r2, r4 }
+  // r4 -> r4
+  // we do not use r3 on sh4 which is by reserved for assembly functions
+
+  // Some preconditions, make the code selection simpler
+  // If these fail, verify and adapt the code below.
+  STATIC_ASSERT(kSmiTagSize == 1);
+  STATIC_ASSERT(kPointerSizeLog2 == 2);
+
+  // Check if the calling frame is an arguments adaptor frame.
+  Label adaptor_frame, try_allocate, runtime;
+  // Pseudo code {
+  //   live-in: none
+  //   live-out: r2
+  //   r2 = load32(add32(fp, kCallerFPOffset))
+  //   r3 = load32(add32(r2, kContextOffset))
+  //   t0 = r3 == ARGUMENTS_ADAPTOR
+  //   if (t0) goto adaptor_frame
+  // }
+  // ARM code {
+  // __ ldr(r2, MemOperand(fp, StandardFrameConstants::kCallerFPOffset));
+  // __ ldr(r3, MemOperand(r2, StandardFrameConstants::kContextOffset));
+  // __ cmp(r3, Operand(Smi::FromInt(StackFrame::ARGUMENTS_ADAPTOR)));
+  // __ b(eq, &adaptor_frame);
+  // }
+  // SH4 code {
+  __ mov(r2, MemOperand(fp, StandardFrameConstants::kCallerFPOffset));
+  __ mov(r0, MemOperand(r2, StandardFrameConstants::kContextOffset));
+  __ mov(r1, Immediate(Smi::FromInt(StackFrame::ARGUMENTS_ADAPTOR)));
+  __ cmpeq(r0, r1);
+  __ bt(&adaptor_frame);
+  // }
+
+  // Get the length from the frame.
+  // Pseudo code {
+  //   live-in: r2
+  //   live-out: r2, r1
+  //   r1 = load32(add32(sp, 0))
+  //   goto  try_allocate
+  // }
+  // ARM code {
+  // __ ldr(r1, MemOperand(sp, 0));
+  // __ b(&try_allocate);
+  // }
+  // SH4 code {
+  __ mov(r1, MemOperand(sp, 0));
+  __ jmp(&try_allocate);
+  // }
+
+  // Patch the arguments.length and the parameters pointer.
+  __ bind(&adaptor_frame);
+  // Pseudo code {
+  //   live-in: r2
+  //   live-out: r2, r1
+  //   r1 = load32(add32(r2, kLengthOffset))
+  //   store32(add32(sp, 0), r1)
+  //   r3 = add32(r2, shl32(r1, kPointerSizeLog2 - kSmiTagSize))
+  //   r3 = add32(r3, kCallerSPOffset)
+  //   store32(add32(sp, 1 * kPointerSize), r3)
+  // }
+  // ARM code {
+  // __ ldr(r1, MemOperand(r2, ArgumentsAdaptorFrameConstants::kLengthOffset));
+  // __ str(r1, MemOperand(sp, 0));
+  // __ add(r3, r2, Operand(r1, LSL, kPointerSizeLog2 - kSmiTagSize));
+  // __ add(r3, r3, Operand(StandardFrameConstants::kCallerSPOffset));
+  // __ str(r3, MemOperand(sp, 1 * kPointerSize));
+  // }
+  // SH4 code {
+  __ mov(r1, MemOperand(r2, ArgumentsAdaptorFrameConstants::kLengthOffset));
+  __ mov(MemOperand(sp, 0), r1);
+  __ lsl(r0, r1, Immediate(kPointerSizeLog2 - kSmiTagSize));
+  __ add(r0, r2, r0);
+  __ add(r0, r0, Immediate(StandardFrameConstants::kCallerSPOffset));
+  __ mov(MemOperand(sp, 1 * kPointerSize), r0);
+  // }
+
+  // Try the new space allocation. Start out with computing the size
+  // of the arguments object and the elements array in words.
+  Label add_arguments_object;
+  __ bind(&try_allocate);
+  // Pseudo code {
+  //   live-in: r2, r1
+  //   live-out: r2, r1
+  //   t0 = r1 == 0
+  //   if (t0) goto add_arguments_object
+  //   r1 = r1 >> kSmiTagSize
+  //   r1 = r1 + FixedArray::kHeaderSize / kPointerSize
+  //   add_arguments_object:
+  //   r1 = r1 + GetArgumentsObjectSize() / kPointerSize
+  // }
+  // ARM code {
+  // __ cmp(r1, Operand(0, RelocInfo::NONE));
+  // __ b(eq, &add_arguments_object);
+  // __ mov(r1, Operand(r1, LSR, kSmiTagSize));
+  // __ add(r1, r1, Operand(FixedArray::kHeaderSize / kPointerSize));
+  // __ bind(&add_arguments_object);
+  // __ add(r1, r1, Operand(GetArgumentsObjectSize() / kPointerSize));
+  // }
+  // SH4 code {
+  __ mov(r0, Immediate(0));
+  __ cmpeq(r1, r0);
+  __ bt(&add_arguments_object);
+  __ lsr(r1, r1, Immediate(kSmiTagSize));
+  __ add(r1, r1, Immediate(FixedArray::kHeaderSize / kPointerSize));
+  __ bind(&add_arguments_object);
+  __ add(r1, r1, Immediate(GetArgumentsObjectSize() / kPointerSize));
+  // }
+  
+  // Do the allocation of both objects in one go.
+  // Pseudo code {
+  //   live-in: r1
+  //   live-out: r0
+  //   AllocateInNewSpace(r1, r0, r2, r3)
+  // }
+  // ARM code {
+  //  __ AllocateInNewSpace(r1, r0, r2, r3, ...)
+  // }
+  // SH4 code {
+  __ AllocateInNewSpace(
+      r1, // object size
+      r0, // result
+      r2, // scratch1
+      r4, // scratch2
+      &runtime,
+      static_cast<AllocationFlags>(TAG_OBJECT | SIZE_IN_WORDS));
+  // }
+
+  // Get the arguments boilerplate from the current (global) context.
+  // Pseudo code {
+  //   live-in: r0
+  //   live-out: r0, r4
+  //   r4 = load32(add32(cp, Context::SlotOffset(Context::GLOBAL_INDEX)))
+  //   r4 = load32(add32(r4, GlobalObject::kGlobalContextOffset))
+  //   r4 = load32(add32(r4, Context::SlotOffset(GetArgumentsBoilerplateIndex())))
+  // }
+  // ARM code {
+  //  __ ldr(r4, MemOperand(cp, Context::SlotOffset(Context::GLOBAL_INDEX)));
+  //  __ ldr(r4, FieldMemOperand(r4, GlobalObject::kGlobalContextOffset));
+  //  __ ldr(r4, MemOperand(r4,
+  //                        Context::SlotOffset(GetArgumentsBoilerplateIndex())));
+  // }
+  // SH4 code {
+  __ mov(r4, MemOperand(cp, Context::SlotOffset(Context::GLOBAL_INDEX)));
+  __ mov(r4, FieldMemOperand(r4, GlobalObject::kGlobalContextOffset));
+  __ mov(r4, MemOperand(r4,
+			Context::SlotOffset(GetArgumentsBoilerplateIndex())));
+  // }
+
+  // Copy the JS object part.
+  // Pseudo code {
+  //   live-in: r0, r4
+  //   live-out: r0
+  //   CopyFields(r0 // dst , r4 //src , r3.bit() // scratch list,
+  //              JSObject::kHeaderSize / kPointerSize)
+  // }
+  // ARM code {
+  //  __ CopyFields(r0, r4, r3.bit(), ...)
+  // }
+  // SH4 code {
+  __ CopyFields(r0, r4, r1.bit(), JSObject::kHeaderSize / kPointerSize);
+  // }
+  if (type_ == NEW_NON_STRICT) {
+    // Setup the callee in-object property.
+    STATIC_ASSERT(Heap::kArgumentsCalleeIndex == 1);
+    const int kCalleeOffset = JSObject::kHeaderSize +
+                              Heap::kArgumentsCalleeIndex * kPointerSize;
+    // Pseudo code {
+    //   live-in: r0
+    //   live-out: r0
+    //   r3 = load32(add32(sp, 2 * kPointerSize))
+    //   store32(add32(r0, kCalleeOffset), r3)
+    // }
+    // ARM code {
+    // __ ldr(r3, MemOperand(sp, 2 * kPointerSize));
+    // __ str(r3, FieldMemOperand(r0, kCalleeOffset));
+    // SH4 code {
+    __ mov(r1, MemOperand(sp, 2 * kPointerSize));
+    __ mov(FieldMemOperand(r0, kCalleeOffset), r1);
+    // }
+  }
+
+  // Get the length (smi tagged) and set that as an in-object property too.
+  STATIC_ASSERT(Heap::kArgumentsLengthIndex == 0);
+  // Pseudo code {
+  //   live-in: r0
+  //   live-out: r0, r1
+  //   r1 = load32(add32(sp, 0 * kPointerSize))
+  //   store32(add32(r0, JSObject::kHeaderSize + Heap::kArgumentsLengthIndex * kPointerSize), r1)
+  // }
+  // ARM code {
+  // __ ldr(r1, MemOperand(sp, 0 * kPointerSize));
+  // __ str(r1, FieldMemOperand(r0, JSObject::kHeaderSize +
+  //                               Heap::kArgumentsLengthIndex * kPointerSize));
+  // }
+  // SH4 code {
+  __ mov(r1, MemOperand(sp, 0 * kPointerSize));
+  __ mov(FieldMemOperand(r0, JSObject::kHeaderSize +
+			 Heap::kArgumentsLengthIndex * kPointerSize), r1);
+  // }
+  // If there are no actual arguments, we're done.
+  Label done;
+  // Pseudo code {
+  //   live-in: r1, r0
+  //   live-out: r1, r0
+  //   t0 = r1 == 0
+  //   if (t0) goto done
+  // }
+  // ARM code {
+  // __ cmp(r1, Operand(0, RelocInfo::NONE));
+  // __ b(eq, &done);
+  // }
+  // SH4 code {
+  __ mov(r2, Immediate(0));
+  __ cmpeq(r1, r2);
+  __ bt(&done);
+  // }
+
+  // Get the parameters pointer from the stack.
+  // Pseudo code {
+  //   live-in: r1, r0
+  //   live-out: r1, r0, r2
+  //   r2 = load32(add32(sp, 1 * kPointerSize))
+  // }
+  // ARM code {
+  // __ ldr(r2, MemOperand(sp, 1 * kPointerSize));
+  // }
+  // SH4 code {
+  __ mov(r2, MemOperand(sp, 1 * kPointerSize));
+  // }
+
+  // Setup the elements pointer in the allocated arguments object and
+  // initialize the header in the elements fixed array.
+  // Pseudo code {
+  //   live-in: r1, r0, r2
+  //   live-out: r1, r0, r2
+  //   r4 = add32(r0, GetArgumentsObjectSize())
+  //   store32(add32(r0, JSObject::kElementsOffset), r4)
+  //   LoadRoot(r3, Heap::kFixedArrayMapRootIndex);
+  //   store32(add32(r4, FixedArray::kMapOffset), r3)
+  //   store32(add32(r4, FixedArray::kLengthOffset), r1)
+  //   r1 = shr(r1, kSmiTagSize)
+  // }
+  // ARM code {
+  // __ add(r4, r0, Operand(GetArgumentsObjectSize()));
+  // __ str(r4, FieldMemOperand(r0, JSObject::kElementsOffset));
+  // __ LoadRoot(r3, Heap::kFixedArrayMapRootIndex);
+  // __ str(r3, FieldMemOperand(r4, FixedArray::kMapOffset));
+  // __ str(r1, FieldMemOperand(r4, FixedArray::kLengthOffset));
+  // __ mov(r1, Operand(r1, LSR, kSmiTagSize));  // Untag the length for the loop.
+  // }
+  // SH4 code {
+  __ add(r4, r0, Immediate(GetArgumentsObjectSize()));
+  __ mov(FieldMemOperand(r0, JSObject::kElementsOffset), r4);
+  __ LoadRoot(r5, Heap::kFixedArrayMapRootIndex);
+  __ mov(FieldMemOperand(r4, FixedArray::kMapOffset), r5);
+  __ mov(FieldMemOperand(r4, FixedArray::kLengthOffset), r1);
+  __ lsr(r1, r1, Immediate(kSmiTagSize));
+  // }
+
+  // Copy the fixed array slots.
+  Label loop;
+  // Setup r4 to point to the first array slot.
+  // Pseudo code {
+  //   live-in: r1, r0, r2, r4
+  //   live-out: r1, r0, r2, r4
+  //   r4 = add32(r4, FixedArray::kHeaderSize - kHeapObjectTag)
+  // }
+  // ARM code {
+  // __ add(r4, r4, Operand(FixedArray::kHeaderSize - kHeapObjectTag));
+  // }
+  // SH4 code {
+  __ add(r4, r4, Immediate(FixedArray::kHeaderSize - kHeapObjectTag));
+  // }
+  __ bind(&loop);
+  // Pre-decrement r2 with kPointerSize on each iteration.
+  // Pre-decrement in order to skip receiver.
+  // Post-increment r4 with kPointerSize on each iteration.
+  // Pseudo code {
+  //   live-in: r1, r0, r2, r4
+  //   live-out: r1, r0, r2, r4
+  //   r2 = sub32(r2, kPointerSize)
+  //   r3 = load32(r2)
+  //   store32(r4, r3)
+  //   r4 = add32(r4, kPointerSize)
+  //   r1 = sub32(r1, 1)
+  //   t0 = r1 == 0
+  //   if (!t0) goto loop
+  // }
+  // ARM code {
+  // __ ldr(r3, MemOperand(r2, kPointerSize, NegPreIndex));
+  // __ str(r3, MemOperand(r4, kPointerSize, PostIndex));
+  // __ sub(r1, r1, Operand(1));
+  // __ cmp(r1, Operand(0, RelocInfo::NONE));
+  // __ b(ne, &loop);
+  // }
+  // SH4 code {
+  __ sub(r2, r2, Immediate(kPointerSize));
+  __ mov(r5, MemOperand(r2, 0));
+  __ mov(MemOperand(r4, 0), r5);
+  __ add(r4, r4, Immediate(kPointerSize));
+  __ sub(r1, r1, Immediate(1));
+  __ mov(r5, Immediate(0));
+  __ cmpeq(r1, r5);
+  __ bf(&loop);
+  // }
+
+  // Return and remove the on-stack parameters.
+  __ bind(&done);
+  // Pseudo code {
+  //   live-in: none
+  //   live-out: none
+  //   sp = sp + 3 * kPointerSize
+  //   return
+  // }
+  // ARM code {
+  //  __ add(sp, sp, Operand(3 * kPointerSize));
+  //  __ Ret()
+  // }
+  // SH4 code {
+  __ add(sp, sp, Immediate(3 * kPointerSize));
+  __ Ret();
+  // }
+
+  // Do the runtime call to allocate the arguments object.
+  __ bind(&runtime);
+  // Pseudo code {
+  //   live-in: none
+  //   live-out: none
+  //   TailCallRuntime
+  // }
+  // ARM code {
+  //  __ TailCallRuntime(Runtime::kNewArgumentsFast, 3, 1);
+  // }
+  // SH4 code {
+  __ TailCallRuntime(Runtime::kNewArgumentsFast, 3, 1);
+  // }
 }
 
 
