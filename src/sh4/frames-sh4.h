03722d4d6c62590eae2045126335beb5afde3996
Rémi Duraffort <remi.duraffort@st.com>
2011-05-04 13:38:15 +0200

    sh4: fix constant ordering (for the stack layout).

--- src/sh4/frames-sh4.h
+++ src/sh4/frames-sh4.h
@@ -76,8 +76,8 @@ static const int kNumSafepointRegisters = 8;
 class StackHandlerConstants : public AllStatic {
  public:
   static const int kNextOffset  = 0 * kPointerSize;
-  static const int kFPOffset    = 1 * kPointerSize;
-  static const int kStateOffset = 2 * kPointerSize;
+  static const int kStateOffset = 1 * kPointerSize;
+  static const int kFPOffset    = 2 * kPointerSize;
   static const int kPCOffset    = 3 * kPointerSize;
 
   static const int kSize = kPCOffset + kPointerSize;
