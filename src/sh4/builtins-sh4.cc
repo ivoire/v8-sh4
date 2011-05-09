57638165ccff363b1de399cf438cb643927dd8cb
Rémi Duraffort <remi.duraffort@st.com>
2011-05-09 16:43:43 +0200

    sh4: implement Builtins::Generate_ArgumentsAdaptorTrampoline

--- src/sh4/builtins-sh4.cc
+++ src/sh4/builtins-sh4.cc
@@ -155,8 +155,141 @@ void Builtins::Generate_FunctionApply(MacroAssembler* masm) {
 }
 
 
+static void EnterArgumentsAdaptorFrame(MacroAssembler* masm) {
+  __ lsl(r4, r4, Immediate(kSmiTagSize));
+  __ mov(r3, Immediate(Smi::FromInt(StackFrame::ARGUMENTS_ADAPTOR)));
+  __ Push(r4, r5, r3, fp);
+  __ push(pr);
+  __ add(fp, sp, Immediate(3 * kPointerSize));
+}
+
+
+static void LeaveArgumentsAdaptorFrame(MacroAssembler* masm) {
+  // ----------- S t a t e -------------
+  //  -- r0 : result being passed through
+  // -----------------------------------
+  // Get the number of arguments passed (as a smi), tear down the frame and
+  // then tear down the parameters.
+  __ mov(r1, MemOperand(fp, -3 * kPointerSize));
+  __ mov(sp, fp);
+  __ Pop(pr, fp);
+  __ lsl(r1, r1, Immediate(kPointerSizeLog2 - kSmiTagSize));
+  __ add(sp, sp, r1);
+  __ add(sp, sp, Immediate(kPointerSize));  // adjust for receiver
+}
+
+
 void Builtins::Generate_ArgumentsAdaptorTrampoline(MacroAssembler* masm) {
-  UNIMPLEMENTED();
+  // ----------- S t a t e -------------
+  //  -- r4 : actual number of arguments
+  //  -- r5 : function (passed through to callee)
+  //  -- r6 : expected number of arguments
+  //  -- r7 : code entry to call
+  // -----------------------------------
+  // arm -> sh4
+  // r0 -> r4
+  // r1 -> r5
+  // r2 -> r6
+  // r3 -> r7
+
+  Label invoke, dont_adapt_arguments;
+
+  Label enough, too_few;
+  __ cmpge(r4, r6);     // actual number of argument lower than expected number
+  __ bf(&too_few);
+  __ mov(r3, Immediate(SharedFunctionInfo::kDontAdaptArgumentsSentinel));
+  __ cmpeq(r6, r3);
+  __ bt(&dont_adapt_arguments);
+
+  {  // Enough parameters: actual >= expected
+    __ bind(&enough);
+    EnterArgumentsAdaptorFrame(masm);
+
+    // Calculate copy start address into r0 and copy end address into r2.
+    // r4: actual number of arguments as a smi
+    // r5: function
+    // r6: expected number of arguments
+    // r7: code entry to call
+    __ lsl(r4, r4, Immediate(kPointerSizeLog2 - kSmiTagSize));
+    __ add(r4, fp, r4);
+    // adjust for return address and receiver
+    __ add(r4, r4, Immediate(2 * kPointerSize));
+    __ lsl(r6, r6, Immediate(kPointerSizeLog2));
+    __ sub(r6, r4, r6);
+
+    // Copy the arguments (including the receiver) to the new stack frame.
+    // r4: copy start address
+    // r5: function
+    // r6: copy end address
+    // r7: code entry to call
+
+    Label copy;
+    __ bind(&copy);
+    __ mov(r3, MemOperand(r4, 0));
+    __ push(r3);
+    __ cmpeq(r4, r6);  // Compare before moving to next argument.
+    __ sub(r4, r4, Immediate(kPointerSize));
+    __ bf(&copy);
+
+    __ jmp(&invoke);
+  }
+
+  {  // Too few parameters: Actual < expected
+    __ bind(&too_few);
+    EnterArgumentsAdaptorFrame(masm);
+
+    // Calculate copy start address into r0 and copy end address is fp.
+    // r4: actual number of arguments as a smi
+    // r5: function
+    // r6: expected number of arguments
+    // r7: code entry to call
+    __ lsl(r4, r4, Immediate(kPointerSizeLog2 - kSmiTagSize));
+    __ add(r4, fp, r4);
+
+    // Copy the arguments (including the receiver) to the new stack frame.
+    // r4: copy start address
+    // r5: function
+    // r6: expected number of arguments
+    // r7: code entry to call
+    Label copy;
+    __ bind(&copy);
+    // Adjust load for return address and receiver.
+    __ mov(r2, MemOperand(r4, 2 * kPointerSize));
+    __ push(r2);
+    __ cmpeq(r4, fp);  // Compare before moving to next argument.
+    __ sub(r4, r4, Immediate(kPointerSize));
+    __ bf(&copy);
+
+    // Fill the remaining expected arguments with undefined.
+    // r5: function
+    // r6: expected number of arguments
+    // r7: code entry to call
+    __ LoadRoot(r3, Heap::kUndefinedValueRootIndex);
+    __ lsl(r6, r6, Immediate(kPointerSizeLog2));
+    __ sub(r6, fp, r6);
+    __ sub(r6, r6, Immediate(4 * kPointerSize));  // Adjust for frame.
+
+    Label fill;
+    __ bind(&fill);
+    __ push(r2);        // kept from the previous load
+    __ cmpeq(sp, r6);
+    __ bf(&fill);
+  }
+
+  // Call the entry point.
+  __ bind(&invoke);
+  __ jsr(r3);
+  // Exit frame and return.
+  LeaveArgumentsAdaptorFrame(masm);
+  __ rts();
+
+
+  // -------------------------------------------
+  // Dont adapt arguments.
+  // -------------------------------------------
+  __ bind(&dont_adapt_arguments);
+  __ mov(r0, r7);
+  __ rts();
 }
 
 
