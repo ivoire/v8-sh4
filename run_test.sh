2b1e3c9cdd53a4fa5e418965f02a47397736d2fd
Rémi Duraffort <remi.duraffort@st.com>
2011-05-25 14:52:21 +0200

    tst: add a timeout for each test to avoid hangings.

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-CCTEST="env QEMU_ASSUME_KERNEL=2.6.30 /home/compwork/guillon/qemu-stm/build-x86_64/sh4-linux-user/qemu-sh4 -distro -L /home/compwork/projects/stlinux/opt/STM/STLinux-2.3/devkit/sh4/target  -x $PWD -cwd $PWD ./obj/test/debug/cctest"
+CCTEST="env QEMU_ASSUME_KERNEL=2.6.30 /sw/st/gnu_compil/gnu/linux-rh-ws-4/bin/timeout 20 /home/compwork/guillon/qemu-stm/build-x86_64/sh4-linux-user/qemu-sh4 -distro -L /home/compwork/projects/stlinux/opt/STM/STLinux-2.3/devkit/sh4/target  -x $PWD -cwd $PWD ./obj/test/debug/cctest"
 
 echo "Listing the test..."
 tests=$($CCTEST --list 2>&1 | grep '<' | sed 's/<$//')
@@ -11,7 +11,7 @@ for name in $tests
 do
   echo "=== $name ==="
   $CCTEST $name > /dev/null 2>&1
-  if [ $? = 255 ]
+  if [ $? != 0 ]
   then
     echo "...FAILED"
     failed=$((failed+1))
