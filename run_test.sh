ddbc3a9cfa8a7d5718711faf552ad7febbdfd9b9
Rémi Duraffort <remi.duraffort@st.com>
2011-05-25 18:20:58 +0200

    Improve the script that runs the tests

--- run_test.sh
+++ run_test.sh
@@ -2,7 +2,15 @@
 CCTEST="env QEMU_ASSUME_KERNEL=2.6.30 /sw/st/gnu_compil/gnu/linux-rh-ws-4/bin/timeout 20 /home/compwork/guillon/qemu-stm/build-x86_64/sh4-linux-user/qemu-sh4 -distro -L /home/compwork/projects/stlinux/opt/STM/STLinux-2.3/devkit/sh4/target  -x $PWD -cwd $PWD ./obj/test/debug/cctest"
 
 echo "Listing the test..."
-tests=$($CCTEST --list 2>&1 | grep '<' | sed 's/<$//')
+tests=$($CCTEST --list 2>&1)
+
+if [ $? != 0 ]
+then
+  echo "ERROR: Unable to list the tests"
+  exit 1
+fi
+
+tests=$(echo $tests | grep '<' | sed 's/<$//')
 total=0
 failed=0
 
