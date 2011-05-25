97c565e8deba10d1bbad94237a83e4bd009d9477
Rémi Duraffort <remi.duraffort@st.com>
2011-05-25 19:14:02 +0200

    sh4: add a small test for sh4

---
+++ test/cctest/test-sh4.cc
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+#include "v8.h"
+
+#include "platform.h"
+#include "cctest.h"
+
+using namespace v8::internal;
+
+
+TEST(sh4) {
+  // Disable compilation of natives.
+  i::FLAG_disable_native_files = true;
+  i::FLAG_full_compiler = false;
+
+  v8::HandleScope scope;
+  LocalContext env;  // from cctest.h
+
+  const char* c_source = "function foo() { return 0x1234; }; foo();";
+  v8::Handle<v8::String> source = v8::String::New(c_source);
+  v8::Handle<v8::Script> script = v8::Script::Compile(source);
+  CHECK_EQ(0x1234,  script->Run()->Int32Value());
+}
+
