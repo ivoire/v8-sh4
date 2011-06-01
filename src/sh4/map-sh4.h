52f5f5d2c5587371117eb5424763f47c4929eb74
Rémi Duraffort <remi.duraffort@st.com>
2011-06-01 15:30:52 +0200

    sh4: only map r8 (not used by ARM), ip and lr.

--- src/sh4/map-sh4.h
+++ src/sh4/map-sh4.h
@@ -4,16 +4,7 @@
 #ifndef MAP_SH4
 // First include, we define the mapping
 #define MAP_SH4
-#define r0 sh4_r0
-#define r1 sh4_r1
-#define r2 sh4_r2
-#define r3 sh4_r10
-#define r4 sh4_r4
-#define r5 sh4_r5
-#define r6 sh4_r6
-#define r7 sh4_r7
 #define r8 "should be cp"
-#define r9 sh4_r9
 #define ip sh4_r11
 #define lr pr
 #define pc "to be checked"
@@ -26,16 +17,7 @@
 #else
 // Second include, we define the mapping
 #undef MAP_SH4
-#undef r0
-#undef r1
-#undef r2
-#undef r3
-#undef r4
-#undef r5
-#undef r6
-#undef r7
 #undef r8
-#undef r9
 #undef ip
 #undef lr
 #undef pc
