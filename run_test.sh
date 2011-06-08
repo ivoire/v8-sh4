cf96eb3f1aec2c636a459ce5cc72b84fd1eb47bc
Christophe Guillon <christophe.guillon@st.com>
2011-06-08 13:04:36 +0200

    sh4: added filter option to run_test.sh.
    If an argument is given to run_test.sh it is used for selecting the tests
    to run.
    For instance: ./run_test.sh sh4
    will run all tests containing "sh4" in their name.

--- run_test.sh
+++ run_test.sh
@@ -1,19 +1,34 @@
 #!/bin/sh
 set -e
+
+[ "$DEBUG" = "" ] || set -x
+
+error() {
+    echo "error: $*" >&2
+    exit 1
+}
+
+cleanup() {
+    [ "$tmpfile" = "" ] || rm -f $tmpfile
+}
+
+trap "cleanup" 0 1 2 15
+
+tmpfile=`mktemp /tmp/cctestXXXXXX`
+
 RUN_PREFIX=${RUN_PREFIX:-"env QEMU_ASSUME_KERNEL=2.6.30 /sw/st/gnu_compil/gnu/linux-rh-ws-4/bin/timeout 20 /home/compwork/guillon/qemu-stm/build-x86_64/sh4-linux-user/qemu-sh4 -distro -L /home/compwork/projects/stlinux/opt/STM/STLinux-2.3/devkit/sh4/target  -x $PWD -cwd $PWD"}
 
 CCTEST="$RUN_PREFIX ./obj/test/debug/cctest"
+$CCTEST --list >$tmpfile 2>&1 || error "cannot list the tests: cctest --list failed"
 
-echo "Listing the test..."
-tests=$($CCTEST --list 2>&1)
-
-if [ $? != 0 ]
-then
-  echo "ERROR: Unable to list the tests"
-  exit 1
+echo "Listing the available tests..."
+if [ $# != 0 ]; then
+    echo "Filtering tests with regexp: $1"
+    tests=$(cat $tmpfile | grep '<$' | grep "$1" | sed 's/<//g')
+else
+    tests=$(cat $tmpfile | grep '<$' | sed 's/<//g')
 fi
 
-tests=$(echo $tests | grep '<' | sed 's/<//g')
 total=0
 failed=0
 
