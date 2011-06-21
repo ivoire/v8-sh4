a2a97785e525f6fcf60a05c57f56eacb0bdd9e33
Christophe Guillon <christophe.guillon@st.com>
2011-06-21 18:10:30 +0200

    sh4: added regression tests for Push/Pop and Ldrd/Strd.

--- test/cctest/test-macro-assembler-sh4.cc
+++ test/cctest/test-macro-assembler-sh4.cc
@@ -48,6 +48,24 @@ static void InitializeVM() {
   }
 }
 
+// We allow use of callee saved r8-r11 for the regression tests
+#define PROLOGUE() \
+  do { \
+  __ push(r8); \
+  __ push(r9); \
+  __ push(r10); \
+  __ push(r11); \
+  } while (0)
+
+#define EPILOGUE() \
+  do { \
+  __ pop(r11); \
+  __ pop(r10);	\
+  __ pop(r9); \
+  __ pop(r8); \
+  } while (0)
+
+
 #define BEGIN()                                         \
   /* Disable compilation of natives. */                 \
   i::FLAG_disable_native_files = true;                  \
@@ -83,6 +101,8 @@ TEST(sh4_ma_0) {
 
   Label error;
 
+  PROLOGUE();
+
   // Verify Move with Immediate
   __ Move(r0, Handle<Object>((Object *)0x12345678));
   __ mov(r1, Immediate(0x12345678));
@@ -109,10 +129,12 @@ TEST(sh4_ma_0) {
   
   // All ok.
   __ mov(r0, Immediate(0));
+  EPILOGUE();
   __ rts();
 
   __ bind(&error);
   __ mov(r0, r10);
+  EPILOGUE();
   __ rts();
 
   JIT();
@@ -130,6 +152,8 @@ TEST(sh4_ma_1) {
   BEGIN();
 
   Label error;
+
+  PROLOGUE();
 
   // Verify Bfc
   __ mov(r0, Immediate(~0x01233210)); // encodes 0xfdeccdef
@@ -179,10 +203,12 @@ TEST(sh4_ma_1) {
 
   // All ok.
   __ mov(r0, Immediate(0));
+  EPILOGUE();
   __ rts();
 
   __ bind(&error);
   __ mov(r0, r10);
+  EPILOGUE();
   __ rts();
 
   JIT();
@@ -195,11 +221,45 @@ TEST(sh4_ma_1) {
   CHECK_EQ(0, res);
 }
 
-// Test Pop/Push
+// Test Stack Pointer changes
 TEST(sh4_ma_2) {
   BEGIN();
 
   Label error;
+
+  PROLOGUE();
+
+  __ mov(r8, sp);
+
+  __ add(sp, sp, Immediate(-16));
+
+  __ mov(sp, r8);
+    
+  __ mov(r0, Immediate(0));
+
+  EPILOGUE();
+  __ rts();
+  
+  JIT();
+#ifdef DEBUG
+  Code::cast(code)->Print();
+#endif
+
+  int res = FUNCTION_CAST<F0>(Code::cast(code)->entry())();
+  ::printf("f() = %d\n", res);
+  CHECK_EQ(0, res);
+}
+
+
+// Test Pop/Push
+TEST(sh4_ma_3) {
+  BEGIN();
+
+  Label error;
+
+  PROLOGUE();
+
+  __ mov(r8, sp);
 
   // Verify Pop and Push
   __ mov(r0, Immediate(3));
@@ -246,11 +306,83 @@ TEST(sh4_ma_2) {
 
 
   // All ok.
+  __ mov(sp, r8);
   __ mov(r0, Immediate(0));
+  EPILOGUE();
   __ rts();
 
   __ bind(&error);
+  __ mov(sp, r8);
   __ mov(r0, r10);
+  EPILOGUE();
+  __ rts();
+
+  JIT();
+#ifdef DEBUG
+  Code::cast(code)->Print();
+#endif
+
+  int res = FUNCTION_CAST<F0>(Code::cast(code)->entry())();
+  ::printf("f() = %d\n", res);
+  CHECK_EQ(0, res);
+}
+
+// Test Ldrd/Strd
+TEST(sh4_ma_4) {
+  BEGIN();
+
+  Label error;
+
+  PROLOGUE();
+
+  __ mov(r8, sp);
+
+  // Verify Pop and Push
+  __ mov(r0, Immediate(3));
+  __ mov(r1, Immediate(5));
+  __ mov(r2, Immediate(7));
+  __ mov(r3, Immediate(11));
+  __ Push(r0,r1,r2,r3);
+  __ mov(r0, Immediate(0xdeadbeef));
+  __ mov(r1, Immediate(0xdeadbeef));
+  __ mov(r2, Immediate(0xdeadbeef));
+  __ mov(r3, Immediate(0xdeadbeef));
+
+  __ Ldrd(r0, r1, MemOperand(sp, 8));
+  __ cmp(r1, Immediate(3));
+  B_LINE(ne, &error);
+  __ cmp(r0, Immediate(5));
+  B_LINE(ne, &error);
+
+  __ Ldrd(r2, r3, MemOperand(sp, 0));
+  __ cmp(r2, Immediate(11));
+  B_LINE(ne, &error);
+  __ cmp(r3, Immediate(7));
+  B_LINE(ne, &error);
+
+  __ Strd(r0, r1, MemOperand(sp, 0));
+  __ Strd(r2, r3, MemOperand(sp, 8));
+  __ Pop(r1,r0);
+  __ Pop(r3,r2);
+  __ cmp(r0, Immediate(5));
+  B_LINE(ne, &error);
+  __ cmp(r1, Immediate(3));
+  B_LINE(ne, &error);
+  __ cmp(r2, Immediate(11));
+  B_LINE(ne, &error);
+  __ cmp(r3, Immediate(7));
+  B_LINE(ne, &error);
+  
+  // All ok.
+  __ mov(sp, r8);
+  __ mov(r0, Immediate(0));
+  EPILOGUE();
+  __ rts();
+
+  __ bind(&error);
+  __ mov(sp, r8);
+  __ mov(r0, r10);
+  EPILOGUE();
   __ rts();
 
   JIT();
