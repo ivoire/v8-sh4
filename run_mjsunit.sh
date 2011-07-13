2537741333a8615bbc90eef10ca4b427d59b308d
Christophe Guillon <christophe.guillon@st.com>
2011-07-13 08:39:43 +0200

    sh4: run_msjunit.sh can take an argument.

--- run_mjsunit.sh
+++ run_mjsunit.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+set -e
+
+[ "$DEBUG" = "" ] || set -x
+
+. ./source_sh4.sh
+
 QEMU=${QEMU-/home/compwork/guillon/qemu-stm/build-x86_64/devimage/bin/qemu-sh4}
 TIMEOUT=${TIMEOUT-"/sw/st/gnu_compil/gnu/linux-rh-ws-4/bin/timeout 600"}
 TARGET_ROOT=${TARGET_ROOT-/home/compwork/projects/stlinux/opt/STM/STLinux-2.3/devkit/sh4/target}
@@ -10,9 +16,11 @@ SHELL="$RUN_PREFIX ./shell_g"
 total=0
 failed=0
 
+files=${1:-"$(ls test/mjsunit/*.js)"}
+
 rm -f run_mjsunit.log
 echo "Running the tests..."
-for file in $(ls test/mjsunit/*.js)
+for file in $files
 do
   echo "=== $file ===" | tee -a run_mjsunit.log
   res=0
