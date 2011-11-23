5b9cd88c1b99416fe4890f1da0c73ef1e53ec1c2
Christophe Guillon <christophe.guillon@st.com>
2011-11-23 20:04:54 +0100

    run_shell_g.sh: fixed run shell with qemu.

--- run_shell_g.sh
+++ run_shell_g.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 #PATH=/opt/qemu-stm/bin/:/opt/qemu-stm/bin/:/usr/local/bin:/usr/bin:/bin:/usr/games:/usr/lib/java/bin:/usr/lib/kde4/libexec LD_LIBRARY_PATH=/opt/qemu-stm/lib /sw/st/gnu_compil/gnu/linux-rh-ws-4/bin/proot -W -Q qemu-sh4 /home/compwork/projects/stlinux/opt/STM/STLinux-2.3/devkit/sh4/target/ ./shell_g
-/home/compwork/projects/proot/release/v0.6/x86_64/proot -W -Q /home/compwork/projects/qemu/release/r0/x86_64/qemu-sh4 /home/compwork/projects/stlinux/opt/STM/STLinux-2.3/devkit/sh4/target/ ./shell_g
+QEMU=${QEMU-/home/compwork/guillon/qemu-stm/build-x86_64/devimage/bin/qemu-sh4}
+TARGET_ROOT=${TARGET_ROOT-/home/compwork/projects/stlinux/opt/STM/STLinux-2.3/devkit/sh4/target}
+RUN_PREFIX=${RUN_PREFIX-"env QEMU_ASSUME_KERNEL=2.6.30 $QEMU -distro -L $TARGET_ROOT -x $PWD -cwd $PWD"}
+${RUN_PREFIX} ./shell_g
