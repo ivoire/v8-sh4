c7554ae428c695a5bff8b7cae2b36ad3d80366d4
Christophe Guillon <christophe.guillon@st.com>
2011-12-16 15:20:01 +0100

    tools/stm: useg sh4 gcc 4.5.3 instead of 4.5.2. This fixed the issue with cctes/test-fast-dtoa.

--- tools/stm/source_site_stgnb.sh
+++ tools/stm/source_site_stgnb.sh
@@ -3,7 +3,7 @@
 #
 export PATH=/home/compwork/projects/scons/`/sw/st/gnu_compil/gnu/scripts/guess-os`/bin:$PATH
 export TIMEOUT=${TIMEOUT:-/sw/st/gnu_compil/gnu/linux-rh-ws-4/bin/timeout}
-export SH4_TOOL_PREFIX=${SH4_TOOL_PREFIX:-/sw/st/gnu_compil/comp/st40/st40-linux/4.5.2-76/bin/sh4-linux}
+export SH4_TOOL_PREFIX=${SH4_TOOL_PREFIX:-/sw/st/gnu_compil/comp/st40/st40-linux/4.5.3-90/bin/sh4-linux}
 export SH4_TARGET_ROOT=${SH4_TARGET_ROOT:-/home/compwork/projects/stlinux/opt/STM/STLinux-2.3/devkit/sh4/target}
 export SH4_QEMU=${SH4_QEMU:-"/home/compwork/guillon/qemu-stm/build-x86_64/devimage/bin/qemu-sh4 -R"}
 export QEMU_ASSUME_KERNEL=2.6.30
