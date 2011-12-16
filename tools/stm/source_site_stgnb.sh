f784b60da11c897538e3f33cab54a7aed9050b4a
Christophe Guillon <christophe.guillon@st.com>
2011-12-16 19:35:06 +0100

    tools/stm: added comments for proot usage for sh4.

--- tools/stm/source_site_stgnb.sh
+++ tools/stm/source_site_stgnb.sh
@@ -3,6 +3,14 @@
 #
 export PATH=/home/compwork/projects/scons/`/sw/st/gnu_compil/gnu/scripts/guess-os`/bin:$PATH
 export TIMEOUT=${TIMEOUT:-/sw/st/gnu_compil/gnu/linux-rh-ws-4/bin/timeout}
+
+# For now proot/qemu on STLinux2.4 does not work correctly 
+#export SH4_TOOL_PREFIX=${SH4_TOOL_PREFIX:-/home/compwork/projects/stlinux/opt/STM/STLinux-2.4/devkit/sh4/bin/sh4-linux}
+#export SH4_TARGET_ROOT=${SH4_TARGET_ROOT:-/home/compwork/projects/stlinux/opt/STM/STLinux-2.4/devkit/sh4/target}
+#export SH4_PROOT=${SH4_PROOT:-/home/compwork/guillon/proot/build-x86_64/devimage/bin/proot}
+#export SH4_QEMU=${SH4_QEMU:-"/home/compwork/guillon/qemu-stm/build-next-x86_64/devimage/bin/qemu-sh4"}
+
+# Still use lagacy qemu on STLinux-2.3
 export SH4_TOOL_PREFIX=${SH4_TOOL_PREFIX:-/sw/st/gnu_compil/comp/st40/st40-linux/4.5.3-90/bin/sh4-linux}
 export SH4_TARGET_ROOT=${SH4_TARGET_ROOT:-/home/compwork/projects/stlinux/opt/STM/STLinux-2.3/devkit/sh4/target}
 export SH4_QEMU=${SH4_QEMU:-"/home/compwork/guillon/qemu-stm/build-x86_64/devimage/bin/qemu-sh4 -R"}
