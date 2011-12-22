4b5d162ffa51da679fe8a3a7370e68774fd1387d
Christophe Guillon <christophe.guillon@st.com>
2011-12-22 21:09:09 +0100

    sh4: enable release mode by default for benchmarks and sputnick.

--- benchmarks/run_benchmarks.sh
+++ benchmarks/run_benchmarks.sh
@@ -4,15 +4,18 @@ set -e
 [ "$DEBUG" = "" ] || set -x
 
 . ../source_sh4.sh
+mode=${mode:-release}
+SHELL=shell_g
+[ "$mode" = release ] && SHELL=shell
+
 rm -f run_benchmarks.log
-cp -f ../shell_g ./
+cp -f ../$SHELL ./
 
 QEMU=${QEMU-/home/compwork/guillon/qemu-stm/build-x86_64/devimage/bin/qemu-sh4}
-TIMEOUT=${TIMEOUT-"/sw/st/gnu_compil/gnu/linux-rh-ws-4/bin/timeout 600"}
+RUN_TIMEOUT=${RUN_TIMEOUT:-1200}
+TIMEOUT=${TIMEOUT-"/sw/st/gnu_compil/gnu/linux-rh-ws-4/bin/timeout $RUN_TIMEOUT"}
 TARGET_ROOT=${TARGET_ROOT-/home/compwork/projects/stlinux/opt/STM/STLinux-2.3/devkit/sh4/target}
 RUN_PREFIX=${RUN_PREFIX-"env QEMU_ASSUME_KERNEL=2.6.30 $TIMEOUT $QEMU -distro -L $TARGET_ROOT -x $PWD -cwd $PWD"}
 
 
-SHELL="$RUN_PREFIX ./shell_g"
-
-$SHELL run.js > run_benchmarks.log
+$RUN_PREFIX ./$SHELL run.js > run_benchmarks.log
--- test/sputnik/run_sputnik.sh
+++ test/sputnik/run_sputnik.sh
@@ -3,11 +3,14 @@
 SPUTNIK_ZIP='/home/compwork/duraffort/pub/sputniktests-v1.zip'
 
 QEMU=${QEMU-/home/compwork/guillon/qemu-stm/build-x86_64/devimage/bin/qemu-sh4}
-TIMEOUT=${TIMEOUT-"/sw/st/gnu_compil/gnu/linux-rh-ws-4/bin/timeout 600"}
+RUN_TIMEOUT=${RUN_TIMEOUT:-1200}
+TIMEOUT=${TIMEOUT-"/sw/st/gnu_compil/gnu/linux-rh-ws-4/bin/timeout $RUN_TIMEOUT"}
 TARGET_ROOT=${TARGET_ROOT-/home/compwork/projects/stlinux/opt/STM/STLinux-2.3/devkit/sh4/target}
 RUN_PREFIX=${RUN_PREFIX-"env QEMU_ASSUME_KERNEL=2.6.30 $TIMEOUT $QEMU -distro -L $TARGET_ROOT -x $PWD -cwd $PWD"}
 
-SHELL="$RUN_PREFIX ./shell_g"
+mode=${mode:-release}
+SHELL=shell_g
+[ "$mode" = release ] && SHELL=shell
 
 # Get the test suite
 echo "=== Fetching an inflating the test suite ==="
@@ -16,8 +19,8 @@ unzip -q $SPUTNIK_ZIP
 
 # Copy the shell to run
 echo "=== Copying the shell ==="
-cp -f ../../shell_g ./
+cp -f ../../$SHELL ./
 
 # Run the test suite
 echo "=== Running the test suite ==="
-python tools/sputnik.py --command "$SHELL"
+python tools/sputnik.py --command "$RUN_PREFIX $SHELL"
