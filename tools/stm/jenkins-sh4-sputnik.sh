74ffc6c814e2502dd30b0ff27dd275fc95a610af
Rémi Duraffort <remi.duraffort@st.com>
2012-01-17 16:11:03 +0100

    sh4: fix the jenkins script for sputnik
    Ask for the right test suite only one time

--- tools/stm/jenkins-sh4-sputnik.sh
+++ tools/stm/jenkins-sh4-sputnik.sh
@@ -24,7 +24,7 @@ env site=$site arch=$arch mode=$mode regexp=$regexp tools/stm/build.sh
 rm -f /dev/shm/sem.qemu-user.acicecmg
 # Use 272k stack size for mjsunit in release mode, seems ok with QEMU/sh4
 # Note that with a 256kb only stack string-indexof-1 fails.
-env XCCTEST_OPTS="-stack-size 272" suite=sputnik site=$site arch=$arch mode=$mode regexp=$regexp tools/stm/run_test.sh --progress=verbose --timeout-scale=10 ${tests} || true
+env XCCTEST_OPTS="-stack-size 272" site=$site arch=$arch mode=$mode regexp=$regexp tools/stm/run_test.sh --progress=verbose --timeout-scale=10 ${tests} || true
 
 # Generate JUnit test output
 wget -O v8test_to_junit.pl -q ${HUDSON_URL}/job/aci-utils-master/lastStableBuild/artifact/utils/v8test_to_junit.pl
