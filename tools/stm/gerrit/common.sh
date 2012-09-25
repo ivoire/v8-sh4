93b43ece6fed7483aa12c13ec0f03d863fcb4b0a
Christophe Guillon <christophe.guillon@st.com>
2012-09-25 10:21:15 +0200

    tools/stm/gerrit: added branch parameter to push-change.
    Now push-change can be used to push into a branch without
    change to the config.sh.
    Use it for instance with: push-change HEAD mygerritbranch

--- tools/stm/gerrit/common.sh
+++ tools/stm/gerrit/common.sh
@@ -22,8 +22,22 @@ forge_reviewer_options() {
 # Echoes the result parameter.
 forge_remote_parameter() {
     local remote="${1?}"
-    local default="${2?}"
-    git remote show "$remote" >/dev/null 2>&1 || remote="$default"
-    echo "$remote"
+    local protocol="${2?}"
+    local default="${3?}"
+    local gerrit_user=`git config gerrit.name`
+    [ "$gerrit_user" != "" ] && gerrit_user="$gerrit_user@"
+    local default_repos="${protocol}://${gerrit_user}${default}"
+    git remote show "$remote" >/dev/null 2>&1 || remote="$default_repos"
+    echo "$default_repos"
 }
 
+# Take the given remote and branch argument and verify
+# whether the remote branch exists.
+# Returns "" if no remote branch is available with this name.
+forge_remote_branch() {
+    local remote="${1?}"
+    local branch="${2?}"
+    remote_branch=`git ls-remote --heads "${remote}" "${branch}"`
+    [ "$remote_branch" != "" ] || return 0
+    echo "$branch"
+}
