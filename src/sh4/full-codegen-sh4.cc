c18d93b409219bb4097dc4f7cf1cfe583d1d3ccd
Rémi Duraffort <remi.duraffort@st.com>
2013-03-29 10:42:02 +0100

    Revert "sh4: fix over optimistic short branch"
    This reverts commit e2ff1bdae2b814051d72af5dfe5fef285039d766.
    
    In fact the code is not over optimistic but only forget to take constant pool
    into account. By flushing the constant pool before short branches, we can still
    use constant pool and nera jumps.

--- src/sh4/full-codegen-sh4.cc
+++ src/sh4/full-codegen-sh4.cc
@@ -293,7 +293,7 @@ void FullCodeGenerator::Generate(CompilationInfo* info) {
       Label ok;
       __ LoadRoot(ip, Heap::kStackLimitRootIndex);
       __ cmphs(sp, ip);
-      __ bt(&ok);
+      __ bt_near(&ok);
       StackCheckStub stub;
       __ CallStub(&stub);
       __ bind(&ok);
