c16b29cf767533c66f1bc83b839446fb3ea903e4
Rémi Duraffort <remi.duraffort@st.com>
2014-01-21 10:46:12 +0100

    Fix compilation with debuggersupport=off

--- src/full-codegen.cc
+++ src/full-codegen.cc
@@ -876,7 +876,7 @@ void FullCodeGenerator::SetExpressionPosition(Expression* expr) {
     }
   }
 #else
-  CodeGenerator::RecordPositions(masm_, pos);
+  CodeGenerator::RecordPositions(masm_, expr->position());
 #endif
 }
 
