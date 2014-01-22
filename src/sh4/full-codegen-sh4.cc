b684704f897b317506a367aa487d49e02cbf1ced
Rémi Duraffort <remi.duraffort@st.com>
2014-01-22 14:18:39 +0100

    sh4: fix pop order and add some asserts

--- src/sh4/full-codegen-sh4.cc
+++ src/sh4/full-codegen-sh4.cc
@@ -444,12 +444,12 @@ void FullCodeGenerator::EmitReturnSequence() {
       EmitProfilingCounterReset();
       __ bind(&ok);
     }
-  // TODO(stm): is it necessary for SH4?
-//#ifdef DEBUG
-//    // Add a label for checking the size of the code used for returning.
-//    Label check_exit_codesize;
-//    masm_->bind(&check_exit_codesize);
-//#endif
+
+#ifdef DEBUG
+    // Add a label for checking the size of the code used for returning.
+    Label check_exit_codesize;
+    masm_->bind(&check_exit_codesize);
+#endif
     // Make sure that the constant pool is not emitted inside of the return
     // sequence.
     { Assembler::BlockConstPoolScope block_const_pool(masm_);
@@ -460,18 +460,18 @@ void FullCodeGenerator::EmitReturnSequence() {
       __ RecordJSReturn();
       masm_->mov(sp, fp);
       int no_frame_start = masm_->pc_offset();
-      masm_->Pop(lr, fp);
+      masm_->Pop(fp, pr);
       masm_->add(sp, sp, Operand(sp_delta));
       masm_->Ret();
       info_->AddNoFrameRange(no_frame_start, masm_->pc_offset());
     }
 
-//#ifdef DEBUG
-//    // Check that the size of the code used for returning is large enough
-//    // for the debugger's requirements.
-//    ASSERT(Assembler::kJSReturnSequenceInstructions <=
-//           masm_->InstructionsGeneratedSince(&check_exit_codesize));
-//#endif
+#ifdef DEBUG
+    // Check that the size of the code used for returning is large enough
+    // for the debugger's requirements.
+    ASSERT(Assembler::kJSReturnSequenceInstructions <=
+           masm_->InstructionsGeneratedSince(&check_exit_codesize));
+#endif
   }
 }
 
