8728728c51556f2600a73d360edf8814a8d7b9fe
Christophe Guillon <christophe.guillon@st.com>
2014-11-18 14:02:31 +0100

    sh4: reviewed src/sh4/code-stubs-sh4.h
    Reviewed file: src/sh4/code-stubs-sh4.h
    Changes: none

--- src/sh4/code-stubs-sh4.h
+++ src/sh4/code-stubs-sh4.h
@@ -1,31 +1,8 @@
-// Copyright 2011-2012 the V8 project authors. All rights reserved.
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
-// met:
-//
-//     * Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above
-//       copyright notice, this list of conditions and the following
-//       disclaimer in the documentation and/or other materials provided
-//       with the distribution.
-//     * Neither the name of Google Inc. nor the names of its
-//       contributors may be used to endorse or promote products derived
-//       from this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// Copyright 2012 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-#ifndef V8_SH4_CODE_STUBS_SH4_H_
+#ifndef V8_SH4_CODE_STUBS_SH4_H_ // FILE: DIFFWITH: arm, REVIEWEDBY: CG
 #define V8_SH4_CODE_STUBS_SH4_H_
 
 #include "ic-inl.h"
@@ -208,19 +185,19 @@ class RecordWriteStub: public PlatformCodeStub {
   static const int kFirstBranchPos = 2 * Assembler::kInstrSize; // DIFF: codegen
   static const int kSecondBranchPos = 4 * Assembler::kInstrSize; // DIFF: codegen
 
-  static void PatchBranchIntoNop(MacroAssembler* masm, int pos) {
+  static void PatchBranchIntoNop(MacroAssembler* masm, int pos) { // REVIEWEDBY: CG
     // SH4: change bf (branch) into bt (nop) ref to ::Generate()
     masm->instr_at_put(pos, (masm->instr_at(pos) & ~0x200/*bt*/)); // DIFF: codegen
     ASSERT(Assembler::IsBt(masm->instr_at(pos)));
   }
 
-  static void PatchNopIntoBranch(MacroAssembler* masm, int pos) {
+  static void PatchNopIntoBranch(MacroAssembler* masm, int pos) { // REVIEWEDBY: CG
     // SH4: change bt (nop) into bf (branch) ref to ::Generate()
     masm->instr_at_put(pos, (masm->instr_at(pos) | 0x200/*bf*/)); // DIFF: codegen
     ASSERT(Assembler::IsBf(masm->instr_at(pos)));
   }
 
-  static Mode GetMode(Code* stub) {
+  static Mode GetMode(Code* stub) { // REVIEWEDBY: CG
     Instr first_instruction = Assembler::instr_at(stub->instruction_start() +
                                                   kFirstBranchPos);
     Instr second_instruction = Assembler::instr_at(stub->instruction_start() +
@@ -242,7 +219,7 @@ class RecordWriteStub: public PlatformCodeStub {
     return STORE_BUFFER_ONLY;
   }
 
-  static void Patch(Code* stub, Mode mode) { // SAMEAS: arm
+  static void Patch(Code* stub, Mode mode) { // REVIEWEDBY: CG
     MacroAssembler masm(NULL,
                         stub->instruction_start(),
                         stub->instruction_size());
@@ -296,7 +273,7 @@ class RecordWriteStub: public PlatformCodeStub {
     // If we have to call into C then we need to save and restore all caller-
     // saved registers that were not already preserved.  The scratch registers
     // will be restored by other means so we don't bother pushing them here.
-    void SaveCallerSaveRegisters(MacroAssembler* masm, SaveFPRegsMode mode) { // SAMEAS: arm
+    void SaveCallerSaveRegisters(MacroAssembler* masm, SaveFPRegsMode mode) { // REVIEWEDBY: CG
       masm->push(pr); // DIFF: codegen
       masm->pushm(kCallerSaved & ~scratch1_.bit()); // DIFF: codegen
       if (mode == kSaveFPRegs) {
@@ -305,7 +282,7 @@ class RecordWriteStub: public PlatformCodeStub {
     }
 
     inline void RestoreCallerSaveRegisters(MacroAssembler*masm,
-                                           SaveFPRegsMode mode) {
+                                           SaveFPRegsMode mode) { // REVIEWEDBY: CG
       if (mode == kSaveFPRegs) {
         masm->RestoreFPRegs(sp, scratch0_);
       }
