74a114adbc91fecfd55df638b63b316891e370ce
ferranti <denis.ferranti@st.com>
2014-12-17 13:27:45 +0100

    Change default timeout time from 60 to 120 secconds

--- tools/run-tests.py
+++ tools/run-tests.py
@@ -51,7 +51,7 @@ from testrunner.objects import context
 
 ARCH_GUESS = utils.DefaultArch()
 DEFAULT_TESTS = ["mjsunit", "fuzz-natives", "cctest", "message", "preparser"]
-TIMEOUT_DEFAULT = 60
+TIMEOUT_DEFAULT = 120
 TIMEOUT_SCALEFACTOR = {"debug"   : 4,
                        "release" : 1 }
 
